09a05d143ef74f473f4323bdee839a8833178459
Pavel Novik <brn2befree@gmail.com>
2026-02-10 02:41:20 +0300

    move index into the tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello from Expo App.</Text>
+      {/* <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
