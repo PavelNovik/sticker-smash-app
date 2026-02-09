11e940a39b36ab08e3abd8781ad8dfb64b7821d1
Pavel Novik <brn2befree@gmail.com>
2026-02-10 02:40:45 +0300

    create a new tabs layout

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,32 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Tabs } from 'expo-router';
+
+export default function TabsLayout() {
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: '#ffd33d',
+                headerStyle: { backgroundColor: '#25292e' },
+                headerShadowVisible: false,
+                headerTintColor: '#fff',
+                tabBarStyle: {
+                    backgroundColor: '#25292e',
+                },
+            }}
+        >
+            <Tabs.Screen name="index" options={{
+                title: 'Home',
+                // headerLeft: () => <></>,
+                tabBarIcon: ({ color, focused }) => (
+                    <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                ),
+            }} />
+            <Tabs.Screen name="about" options={{
+                title: 'About',
+                tabBarIcon: ({ color, focused }) => (
+                    <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+                ),
+            }} />
+        </Tabs>
+    );
+}
