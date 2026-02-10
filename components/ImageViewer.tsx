1a8f03832528312c218184a521cf0616e45ce863
Pavel Novik <brn2befree@gmail.com>
2026-02-10 11:26:00 +0300

    refactor code in imageViewer

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -3,12 +3,12 @@ import { ImageSourcePropType, StyleSheet } from 'react-native';
 
 type Props = {
     imgSource: ImageSourcePropType;
-    selectedImage?: string;
+    // selectedImage?: string;
 };
 
-export default function ImageViewer({ imgSource, selectedImage }: Props) {
-    const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
-    return <Image source={imageSource} style={styles.image} />;
+export default function ImageViewer({ imgSource }: Props) {
+    // const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+    return <Image source={imgSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
