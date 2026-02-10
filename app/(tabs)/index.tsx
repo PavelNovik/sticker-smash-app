1326bea0eb1615cbf91b777c667767df14aefa7c
Pavel Novik <brn2befree@gmail.com>
2026-02-10 10:54:08 +0300

    add image and buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
+import { StyleSheet, View } from "react-native";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello from Expo App.</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      {/* <Image source={PlaceholderImage} style={styles.image} /> */}
+      {/* <Text style={styles.text}>Hello from Expo App.</Text> */}
       {/* <Link href="/about" style={styles.button}>
         Go to About screen
       </Link> */}
@@ -14,16 +40,29 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    // justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#25292e",
   },
-  text: {
-    color: "#fff",
+  footerContainer: {
+    flex: 1 / 3,
+    // justifyContent: "center",
+    alignItems: "center",
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  // text: {
+  //   color: "#fff",
+  // },
+  // button: {
+  //   fontSize: 20,
+  //   textDecorationLine: 'underline',
+  //   color: '#fff',
+  // },
+  imageContainer: {
+    flex: 1,
   },
+  // image: {
+  //   width: 320,
+  //   height: 440,
+  //   borderRadius: 18,
+  // },
 });
